bf70ea7455efa738193f30b549edc05f5ba9a97c
Christian Mendoza <christian.mendoza@gmail.com>
2013-12-01 17:34:08 -0500

    Complete 'movie_app pt. 2' homework

--- Christian_Mendoza/movies_app/app/controllers/movies_controller.rb
+++ Christian_Mendoza/movies_app/app/controllers/movies_controller.rb
@@ -1,30 +1,52 @@
 class MoviesController < ApplicationController
-  def index
-    @movies = Movie.get_all
-  end
+  before_action :find_movie, only: [:edit, :show, :update, :destroy]
 
-  def new
-    @movie = Movie.instantiate_new
+  def index
+    @movies = Movie.all
   end
 
   def create
-    safe_movie = params.require(:movie).permit(:title, :description, :year_released, :rating)
-    movie = Movie.create safe_movie
-    redirect_to movie
+    @movie = Movie.create safe_movie
+
+    if @movie.save
+      redirect_to @movie, notice: "The movie '#{@movie.title}' was added."
+    else
+      render 'new'
+    end
   end
 
-  def show
-    @movie = Movie.find params[:id]
+  def new
+    @movie = Movie.new
   end
 
   def edit
-    @movie = Movie.find params[:id]
+
+  end
+
+  def show
+  
   end
 
   def update
+    if @movie.update safe_movie
+      redirect_to @movie, notice: "The movie '#{@movie.title}' was updated."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to root_path, alert: "The movie '#{@movie.title}' was deleted."
+  end
+
+  private
+
+  def safe_movie
+    params.require(:movie).permit(:title, :description, :year_released, :rating)
+  end
+
+  def find_movie
     @movie = Movie.find params[:id]
-    safe_movie = params.require(:movie).permit(:title, :description :year_released, :rating)
-    @movie.update safe_movie
-    redirect_to @movie
   end
 end
--- Christian_Mendoza/movies_app/app/models/movie.rb
+++ Christian_Mendoza/movies_app/app/models/movie.rb
@@ -1,43 +1,13 @@
 class Movie < ActiveRecord::Base
   validates :title, presence: true
   validates :description, length: { minimum: 10 }, presence: true
-  validates :year_released, numericality: { greater_than: 0 }
-  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :year_released, numericality: { greater_than: 0 }, allow_blank: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }, allow_blank: true
 
   # All titles and descriptions must be present
   # Descriptions must be greater than 10 characters
   # Year released must be a number greater than 0
   # Rating must be a number between 1 and 5
-
-  def self.create_new
-    new
-  end
-
-  def create
-    safe_movie = params.require(:movie).permit(:title, :description)
-    movie = Movie.create safe_movie
-    redirect_to movie
-  end
-
-  def show
-    @movie = Movie.find params[:id]
-  end
-
-  def edit
-    @movie = Movie.find params[:id]
-  end
-
-  def update
-    @movie = Movie.find params[:id]
-    safe_movie = params.require(:movie).permit(:title, :description)
-    @movie.update safe_movie
-    redirect_to @movie
-  end
-
-  def self.get_all
-    #order(year_released: :asc)
-    readonly.all
-  end
 
   def self.search_for (query)
     where('title LIKE :query OR description LIKE :query OR year_released LIKE :query', query: "%#{query}%")
