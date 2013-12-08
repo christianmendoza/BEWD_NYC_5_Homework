6d38e00bad7335e0f6533d39fe6c14dd9b0bab0f
Christian Mendoza <christian.mendoza@gmail.com>
2013-12-07 20:31:21 -0500

    Add stories#show

--- Christian_Mendoza/rewsly_app/app/controllers/stories_controller.rb
+++ Christian_Mendoza/rewsly_app/app/controllers/stories_controller.rb
@@ -1,5 +1,16 @@
 class StoriesController < ApplicationController
+  before_action :find_story, only: [:show]
+
   def index
     @stories = Story.all
+  end
+
+  def show
+  end
+
+  private
+
+  def find_story
+    @story = Story.find params[:id]
   end
 end
--- Christian_Mendoza/rewsly_app/config/routes.rb
+++ Christian_Mendoza/rewsly_app/config/routes.rb
@@ -1,3 +1,4 @@
 RewslyApp::Application.routes.draw do
   root "stories#index"
+  resources :stories, only: [:index, :show]
 end
